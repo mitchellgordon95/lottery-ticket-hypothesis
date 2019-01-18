987ef2aa984d32d6fe47240162e11d857c9b0a07
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-01-18 14:24:48 -0500

    Use single analysis script instead of many, add one layer exp

--- lottery_ticket/mnist_fc/analyze_masks.py
+++ lottery_ticket/mnist_fc/analyze_masks.py
@@ -1,18 +1,22 @@
 from __future__ import print_function
 
 import os
+import sys
 import subprocess
 import numpy as np
 from lottery_ticket.foundations import paths
 from lottery_ticket.mnist_fc import constants
 
+
 for trial in range(1, 21):
     print('Trial {}'.format(trial))
-    trial_path = paths.trial(constants.EXPERIMENT_PATH, trial, 'structure')
+    trial_path = paths.trial(constants.EXPERIMENT_PATH, trial, sys.argv[1])
 
     first_run_path = paths.run(trial_path, 0)
     first_run_train_acc = subprocess.check_output(['tail', '-n', '1', paths.log(first_run_path, 'train')]).strip().split(',')[-1]
     print('\tFirst run train acc: {}'.format(first_run_train_acc))
+    first_run_test_acc = subprocess.check_output(['tail', '-n', '1', paths.log(first_run_path, 'test')]).strip().split(',')[-1]
+    print('\tFirst run test acc: {}'.format(first_run_test_acc))
 
     runs = map(int, os.listdir(trial_path))
     second_last_run = sorted(runs)[-2]
@@ -20,6 +24,8 @@ for trial in range(1, 21):
 
     second_last_train_acc = subprocess.check_output(['tail', '-n', '1', paths.log(second_last_path, 'train')]).strip().split(',')[-1]
     print('\tSecond to last train acc run ({}): {}'.format(second_last_run, second_last_train_acc))
+    second_last_test_acc = subprocess.check_output(['tail', '-n', '1', paths.log(second_last_path, 'test')]).strip().split(',')[-1]
+    print('\tTest: {}'.format(second_last_test_acc))
     print('\tNonempty\tRows\tColumns\tWeights')
     masks_dir = paths.masks(second_last_path)
     for mask in sorted(os.listdir(masks_dir)):
